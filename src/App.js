68e429b63ff7cf80d3e072406ba28e8ebf79f94c
guilherme.porto <guilherme.porto@lambda3.com.br>
2017-05-25 09:54:03 -0300

    First version of static components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,106 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ProductRow = (props) => {
+  return (
+    <div className='productRow'>
+      <label>{props.name}</label> <label>{props.price}</label>
+    </div>
+  )
+}
+
+const ProductCategoryRow = (props) => {
+  return (
+    <div>
+      <label className='productCategory'>{props.name}</label>
+    </div>
+  )
+}
+
+const ProductTable = (props) => {
+  const mapProductRow = (category, products) => {
+    return products
+      .filter(product => product.category === category)
+      .map((product, index) =>
+        <ProductRow key={index} name={product.name} price={product.price} />
+      )
+  }
+
+  return (
+    <div>
+      <div className='productTableTitle'>
+        <label>Name</label> <label>Price</label>
+      </div>
+      <div className='productTable'>
+        <ProductCategoryRow name='Sporting Goods' />
+        {mapProductRow('Sporting Goods', props.products)}
+        <ProductCategoryRow name='Eletronics' />
+        {mapProductRow('Eletronics', props.products)}
+      </div>
+    </div>
+  )
+}
+
+const SearchBar = (props) => {
+  return (
+    <div>
+      <div>
+        <input type='text' placeholder='Search...' />
+      </div>
+      <div>
+        <input type='checkbox' /> Only show products in stock
+      </div>
+    </div>
+  )
+}
+
+const FilterableProductTable = (props) => {
+  const products = [
+    {
+      category: 'Sporting Goods',
+      price: '$49.99',
+      stocked: true,
+      name: 'Football'
+    },
+    {
+      category: 'Sporting Goods',
+      price: '$9.99',
+      stocked: true,
+      name: 'Baseball'
+    },
+    {
+      category: 'Sporting Goods',
+      price: '$29.99',
+      stocked: false,
+      name: 'Basketball'
+    },
+    {
+      category: 'Eletronics',
+      price: '$99.99',
+      stocked: true,
+      name: 'iPod Touch'
+    },
+    {
+      category: 'Eletronics',
+      price: '$399.99',
+      stocked: false,
+      name: 'iPhone 5'
+    },
+    {
+      category: 'Eletronics',
+      price: '$199.99',
+      stocked: true,
+      name: 'Nexus 7'
+    }
+  ]
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +113,7 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload!
         </p>
+        <FilterableProductTable />
       </div>
     );
   }
